0b5f60a1f6ca20e1335e4901497fb69d3a5881c0
Chandan Aulakh <49302381+chandan1600@users.noreply.github.com>
2021-05-12 18:38:41 +1200

    setup

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Grid  from '@material-ui/core';
+import { Description } from '@material-ui/icons';
+
+const products = {
+    { id: 1, name: 'Shoes', Description: 'Air Jordans.'},
+
+};
+
+const Products = () => {
+    <main>
+        <Grid container justify="center" spacing={4}>
+
+        </Grid>
+    </main>
+}
+
+export default Products;
